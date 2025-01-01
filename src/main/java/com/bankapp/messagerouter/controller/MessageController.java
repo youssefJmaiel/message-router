9259b4173b4d4da8203f8523adda27b0fef8469d
youssef <youssef.jemaiel@nearteam.fr>
2025-01-01 21:55:48 +0100

    nouveau message test 2

--- src/main/java/com/bankapp/messagerouter/controller/MessageController.java
+++ src/main/java/com/bankapp/messagerouter/controller/MessageController.java
@@ -28,11 +28,7 @@ public class MessageController {
         this.messageService = messageService;
     }
 
-//    @GetMapping("/messages")
-//    public ResponseEntity<List<Message>> getAllMessages() {
-//        List<Message> messages = messageService.getAllMessages();
-//        return ResponseEntity.ok(messages);
-//    }
+
 
 
     @GetMapping("/messages")
@@ -68,27 +64,6 @@ public class MessageController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); // 404 Not Found
         }
     }
-
-//
-//    @PostMapping("/message/send")
-//    public ResponseEntity<Message> sendMessage(@Valid @RequestBody MessageRequest messageRequest) {
-//        try {
-//            // Créer et envoyer le message
-//            Message message = messageService.sendMessage(
-//                    messageRequest.getContent(),
-//                    messageRequest.getSender(),
-//                    messageRequest.getReceiver()
-//            );
-//
-//            // Retourner une réponse HTTP 201 avec l'objet message créé
-//            return new ResponseEntity<>(message, HttpStatus.CREATED);
-//
-//        } catch (Exception e) {
-//            // Gérer les erreurs et retourner une réponse HTTP 400
-//            log.error("Failed to send message: {}", e.getMessage(), e);
-//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//        }
-//    }
 
     @PostMapping("/message/send")
     public ResponseEntity<Message> sendMessage(@Valid @RequestBody MessageRequest messageRequest) {
